13f399db157faf5790b05dd9fce35a2ca76d47dd
SupakitPor <supakit.prong@gmail.com>
2026-07-02 11:55:55 +0700

    add food section and background image

---
+++ my-array.js
@@ -0,0 +1,3 @@
+let food = ["pizza", "burger", "sushi", "pasta", "salad"];
+
+console.log(food); // Output: pizza
---
+++ my-object.js
@@ -0,0 +1,37 @@
+// ["pizza", "burger", "sushi", "pasta", "salad"]
+
+let foodDatabase = [
+    { 
+        id: 1, 
+        name: "pizza", 
+        size: [
+            {name: "small", price: "249" },
+            {name: "medium", price: "349" },
+        ],
+        PhotoUrl:"https://img.magnific.com/free-psd/delicious-pepperoni-pizza-culinary-delight_632498-24206.jpg"
+    },
+    { 
+        id: 2, 
+        name: "burger", 
+        price: "149",
+        PhotoUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWnMk-vgOWJqtxvXOC9GjzlxXJcx041C4rHECb8MKRHA&s=10"
+    },
+    { 
+        id: 3, 
+        name: "sushi", 
+        price: "349",
+        PhotoUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQaB_MajR4DOZpiUbi1TvhPMq5HTr_PLBm0srBVovdN7mg4wtPzoMvTYc&s=10"
+    },
+    { 
+        id: 4, 
+        name: "pasta", 
+        price: "199",
+        PhotoUrl:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtcBfVL52H-ehlAMZpzHRcAZptrBwwUpAHpaOJ69aonPnDlxyU2A3bfqc&s=10"
+    },
+    { 
+        id: 5, 
+        name: "salad", 
+        price: "99",
+        PhotoUrl:"https://www.mamypoko.co.in/_next/image?url=https%3A%2F%2Fd1lr9ifk036o6u.cloudfront.net%2F10b16941-5524-492e-ba1f-ef8f66965f8e&w=3840&q=75"
+    }
+];
